424c47de09a2e7e928b9dc61efbc636e11e0137c
nikkiiii <Guest@WTS6.local>
2015-06-13 12:33:45 -0700

    spam

--- src/spamalot.java
+++ src/spamalot.java
@@ -1,4 +1,6 @@
 import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Properties;
 
 import javax.mail.Message;
@@ -11,61 +13,92 @@ import javax.swing.JButton;
 import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
-
-public class spamalot {
+public class spamalot implements ActionListener {
 	static final String FAKE_USERNAME = "hihi14591@gmail.com";
-static final String FAKE_PASSWORD = "Aidan4473";
+	static final String FAKE_PASSWORD = "Aidan4473";
 
-	
-	
-public static void main(String[] args) {
-	spamalot spam=new spamalot();
-	spam.sendSpam("liam.tan8@gmail.com", "stalking u", "stalked");
-	spam.ui();
-}
-private boolean sendSpam(String recipient, String subject, String content) {
+	JButton button1 = new JButton("bad");
 
-	Properties props = new Properties();
-	props.put("mail.smtp.auth", "true");
-	props.put("mail.smtp.starttls.enable", "true");
-	props.put("mail.smtp.host", "smtp.gmail.com");
-	props.put("mail.smtp.port", "587");
+	JButton button2 = new JButton("good");
+	JTextField field = new JTextField(10);
 
-	Session session = Session.getInstance(props, new javax.mail.Authenticator() {
-		protected javax.mail.PasswordAuthentication getPasswordAuthentication() {
-			return new javax.mail.PasswordAuthentication(FAKE_USERNAME, FAKE_PASSWORD);
-		}
-	});
+	public static void main(String[] args) {
+		spamalot spam = new spamalot();
 
-	try {
-
-		Message message = new MimeMessage(session);
-		message.setFrom(new InternetAddress(FAKE_USERNAME));
-		message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(recipient));
-		message.setSubject(subject);
-		message.setText(content);
-		Transport.send(message);
-		return true;
-
-	} catch (MessagingException e) {
-e.printStackTrace();
-return false;
+		spam.ui();
 	}
-}
 
-public void ui(){
-JFrame frame= new JFrame();	
-	JPanel panel=new JPanel();
-	JButton button1=new JButton("bad");
-	JButton button2=new JButton("good");
-	frame.add(panel);
-	panel.add(button1);
-	panel.add(button2);
-	frame.pack();
-    frame.setVisible(true);
-    JComponent myJTextField = null;
-	myJTextField.setBackground(Color.RED);
+	public boolean sendSpam(String recipient, String subject, String content) {
 
-}
+		Properties props = new Properties();
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.smtp.host", "smtp.gmail.com");
+		props.put("mail.smtp.port", "587");
+
+		Session session = Session.getInstance(props,
+				new javax.mail.Authenticator() {
+					protected javax.mail.PasswordAuthentication getPasswordAuthentication() {
+						return new javax.mail.PasswordAuthentication(
+								FAKE_USERNAME, FAKE_PASSWORD);
+					}
+				});
+
+		try {
+
+			Message message = new MimeMessage(session);
+			message.setFrom(new InternetAddress(FAKE_USERNAME));
+			message.setRecipients(Message.RecipientType.TO,
+					InternetAddress.parse(recipient));
+			message.setSubject(subject);
+			message.setText(content);
+			Transport.send(message);
+			return true;
+
+		} catch (MessagingException e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+
+	public void ui() {
+		JFrame frame = new JFrame();
+		JPanel panel = new JPanel();
+
+		frame.add(panel);
+		panel.add(field);
+		panel.add(button1);
+		panel.add(button2);
+		button1.addActionListener((ActionListener) this);
+		button2.addActionListener((ActionListener) this);
+		frame.pack();
+		frame.setVisible(true);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent event) {
+		// TODO Auto-generated method stub
+		boolean success = false;
+
+		if (button1.equals(event.getSource()))
+
+		{
+
+			success = sendSpam(field.getText(), "stalking u",
+					"GIVE ME A MILLION $$ OR I WILL DESTROY EVERYTHING U OWN");
+		} else if (button2.equals(event.getSource()))
+
+		{
+			success = sendSpam(field.getText(), "AIDAN SPAMS","GET SPAMMED GET SPAMMED GET SPAMMED GET SPAMMED GET SPAMMED GET SPAMMED  GET SPAMMED GET SPAMMED GET SPAMMED GET SPAMMED");
+					
+
+		}
+		if (success) {
+			field.setBackground(Color.green);
+		} else {
+			field.setBackground(Color.RED);
+		}
+	}
 }
